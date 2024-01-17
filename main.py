8c426fe1a8f91ca56e53311b945618ac3e092d96
SYNTATIC-REALITY <87398867+SYNTATIC-REALITY@users.noreply.github.com>
2024-01-17 16:16:17 +0100

    This commit add deposit features in main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#Welcome to the Magical Token Machhine. Start the game in the terminal. 
+# Welcome to the Magical Token Machhine. Start the game in the terminal. Run the code.
 
 import random
 
-MAX_TOKENS = 10000
-MAX_SPIN = 5
 
 symbols = ["A", "B", "C", "D"]
 symbols_count = {"A":4, "B":5, "C":10, "D":12}
@@ -16,8 +14,25 @@ class magicMachine:
         self.tokens = 0
         self.total_games = 0
         self.total_wins = 0
+        self.max_lines = 5
         
     def deposit_tokens(self):
         while True:
             try:
+                amount = int(input("How many tokens would you like to deposit? "))
+                if amount > 0:
+                    self.tokens += amount
+                    print(f"You now have {self.tokens} tokens.")                    
+                    break
+                else:
+                    print("Please enter a valid number of tokens.")
+            except ValueError:
+                print("Please enter a valid number of tokens.")
                 
+    def place_bet(self):
+        while True:
+            try:
+                bet = int(input("How many tokens would you like to bet? "))
+            if 0 < bet <= self.tokens:
+                self.tokens -= bet
+                print(f"")
