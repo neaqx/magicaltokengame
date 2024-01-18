5a31d09657f3305fce283f60257e2846a57366b6
SYNTATIC-REALITY <87398867+SYNTATIC-REALITY@users.noreply.github.com>
2024-01-18 13:02:33 +0100

    This commit add bonus rounds in main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Welcome to the Magical Token Machhine. Start the game in the terminal. Run the code.
 
 import random
+import json
 
-symbols = ["A", "B", "C", "D"]
-symbol_count = {"A":4, "B":5, "C":10, "D":12}
-symbol_value = {"A":10, "B":5, "C":2, "D":1}
+symbols = ["A", "B", "C", "D" "E"]
+symbol_count = {"A":4, "B":5, "C":10, "D":12, "E":1}
+symbol_value = {"A":10, "B":5, "C":2, "D":1, "E":0}
 
 class MagicMachine:    
     def __init__(self):
@@ -12,7 +13,9 @@ class MagicMachine:
         self.total_games = 0
         self.total_wins = 0
         self.max_lines = 5
-        
+        self.free_spins = 0
+
+#Deposit tokens to play       
     def deposit_tokens(self):
         while True:
             try:
@@ -25,8 +28,15 @@ class MagicMachine:
                     print("Please enter a positive number.")
             except ValueError:
                     print("Invalid input. Please enter a number.")
+                    
+#Check if player gets bonus spins                   
+    def check_bonus(self):
+        if random.randint(1,10) == 1:
+            bonus_spins = random.randint(1,5)
+            print(f"You have won {bonus_spins} free spins!")
+            self.free_spins += bonus_spins
 
-                
+#Place bet and check if bet is valid               
     def place_bet(self):
         while True:
             try:
@@ -41,16 +51,28 @@ class MagicMachine:
                 print("Invalid bet. Please bet a number")
                 
     def spin(self,bet,lines):
-        print("You have spun the machine!")
+        if self.free_spins > 0:
+            print(f"You have {self.free_spins} free spins left.")
+            self.free_spins -= 1
+        else:
+            self.tokens -= bet
+            print(f"{bet} tokens have been bet. You now have {self.tokens} tokens.")
+            
+            
+            print("You have spun the machine!")
         results = []
         for _ in range(lines):
             spin_result = [random.choice(symbols)for _ in range(3)]
             print(" | ".join(spin_result))
+            if "E" in spin_result:
+                special_bonus = random.randint(5,20)
+                print(f"Special symbol E appeared! You got {special_bonus} extra tokens!")
             if all( s == spin_result[0] for s in spin_result):
                 win = symbol_value[spin_result[0]]*bet
                 print(f"You won! You won {win} tokens!")
                 self.tokens += win
                 self.total_wins += 1
+                self.check_bonus()
             else:
                 print("You lost.")
                 results.append(spin_result)
@@ -100,6 +122,10 @@ class MagicMachine:
 
 
 if __name__ == '__main__':
+    player_name = input("Please enter your name: ")
     game = MagicMachine()
+    game.load_player(player_name)
     game.play() 
+    game.save_player(player_name)
+    
          
