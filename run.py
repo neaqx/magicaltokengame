f772b5bc3cd98e0d81f4f3e50af51d26efb8804f
SYNTATIC-REALITY <87398867+SYNTATIC-REALITY@users.noreply.github.com>
2024-01-19 17:07:40 +0100

    Fix multiline string in input und print

--- run.py
+++ run.py
@@ -18,39 +18,39 @@ class MagicMachine:
     def deposit_tokens(self):
         while True:
             try:
-                deposit_text = """Welcome to the Magical Token Machine. 
-                How many tokens would you like to deposit?"""
-                amount = int(input(deposit_text))
+                deposit_text = """
+Welcome to the Magical Token Machine. 
+How many tokens would you like to deposit?\n 
+                """
+                amount = int(input(deposit_text + '\n'))
                 if amount > 0:
                     self.tokens += amount
-                    amount_text = """f"You have deposited {amount} tokens. 
-                    Total: {self.tokens} tokens"""
-                    print(amount_text)
+                    print(f"\nYou have deposited {amount} tokens. You now have {self.tokens} tokens.\n")
                     break
                 else:
-                    print("Please enter a positive number.")
+                    print("\nPlease enter a positive number.\n")
             except ValueError:
-                    print("Invalid input. Please enter a number.")
+                    print("\nInvalid input. Please enter a number.\n")
 #Deposit tokens to play                    
                  
     def check_bonus(self):
         if random.randint(1,10) == 1:
             bonus_spins = random.randint(1,5)
-            print(f"You have won {bonus_spins} free spins!")
+            print(f"\nYou have won {bonus_spins} free spins!\n")
             self.free_spins += bonus_spins
 #Check if player gets bonus spins  
              
     def place_bet(self):
         while True:
             try:
-                bet = int(input("How many tokens would you like to bet?\n "))
+                bet = int(input("\nHow many tokens would you like to bet?\n "))
                 if 0 < bet <= self.tokens:
-                    print(f"{bet} Tokens have been bet. You now have {self.tokens - bet} tokens.")
+                    print(f"\n{bet} Tokens have been bet. You now have {self.tokens - bet} tokens.\n")
                     return bet
                 else:
-                    print("Invalid bet. Please bet a number between 1 and your total token amount.")
+                    print("\nInvalid bet. Please bet a number between 1 and your total token amount.\n")
             except ValueError:
-                print("Invalid bet. Please bet a number")
+                print("\nInvalid bet. Please bet a number\n")
 #Place bet and check if bet is valid                                 
                
     def spin(self,bet,lines):
@@ -72,7 +72,7 @@ class MagicMachine:
             print(" | ".join(spin_result))
             if "E" in spin_result:
                 special_bonus = random.randint(1,100)
-                print(f"Special symbol E appeared! You got {special_bonus} extra tokens!")
+                print(f"\nSpecial symbol E appeared! You got {special_bonus} extra tokens!\n")
                 self.tokens += special_bonus
             if all(s == spin_result[0] for s in spin_result):
                 win = symbol_value[spin_result[0]]*bet
@@ -104,7 +104,7 @@ class MagicMachine:
             
     def request_payout_or_continue(self):
         while True:
-            choice = input("Would you like to payout or continue? P/C?\n  ").upper()
+            choice = input("Would you like to payout or continue? P/C?\n").upper()
             if choice == "C":
                 return True
             elif choice == "P":
@@ -115,14 +115,16 @@ class MagicMachine:
               
     def reading_rules(self):
         while True:
-            rules = """Welcome to the Magical Token Machine. Here some rules?
-            1.You need to deposit the tokens. 
-            2.You can bet on max.5 lines. 
-            3.Winning condition: Three identical letters in a row!
-            4.Wnning Condition Exmaple: A|A|A, B|B|B, C|C|C or D|D|D!
-            5.SPEICAL BONUS: E Letter for extra tokens or get a rare extra spin!  
-            Do you agree or disagree? A/D"""
-            choice = input(rules).upper()
+            rules = """
+Welcome to the Magical Token Machine. Here some rules?
+1.You need to deposit the tokens.
+2.You can bet on max.5 lines.
+3.Winning condition: Three identical letters in a row!
+4.Wnning Condition Exmaple: A|A|A, B|B|B, C|C|C or D|D|D!
+5.SPEICAL BONUS: E Letter for extra tokens or get a rare extra spin!
+Do you agree or disagree? A/D 
+"""
+            choice = input(rules + '\n').upper()
             if choice == "A":
                 return True
             elif choice == "D":
@@ -139,7 +141,7 @@ class MagicMachine:
         self.deposit_tokens()
         while True:
             bet = self.place_bet()
-            lines = int(input(f"How many lines would you like to play? Max. {self.max_lines}\n "))
+            lines = int(input(f"\nHow many lines would you like to play? Max. {self.max_lines}\n "))
             if 1 <= lines <= self.max_lines:
                 self.spin(bet, lines)
                 if self.tokens <= 0:
@@ -151,7 +153,7 @@ class MagicMachine:
                     self.payout_tokens()
                     break
             else:
-                    print(f"Invalid number of lines. You can set to 1 to {self.max_lines} lines.")
+                    print(f"\nInvalid number of lines. You can set to 1 to {self.max_lines} lines.\n")
 #Play the game  
                            
 if __name__ == '__main__':
